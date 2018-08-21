79785d05b007800332b34d0f3fcec35f20154957
Janet Leon <leon.irenejanet@gmail.com>
2018-08-21 12:17:32 -0700

    worked on higher order functions

--- loops-arrays.js
+++ loops-arrays.js
@@ -1,141 +1,93 @@
 // console.log("Testing");
+'use strict';
 
 let arr = [-5, 54, 28, 98, -20013, 0.7878, 22, 115];
 
 function max(numbers) {
-    let greatNum = numbers[0];
-    let count = 0;
-    while(count < numbers.length - 1){
-        count++;
-        console.log(greatNum);
-        if(greatNum < numbers[count]){
-            greatNum = numbers[count];   
-        } 
-    } return greatNum;
-  }
+  let greatNum = numbers[0];
+  let count = 0;
+  while(count < numbers.length - 1){
+    count++;
+    if(greatNum < numbers[count]){
+      greatNum = numbers[count];   
+    } 
+  } return greatNum;
+}
   
 console.log(max(arr));
 
-  function min(numbers) {
-    let greatNum = numbers[0];
-    let count = 0;
-    while(count < numbers.length - 1){
-        count++;
-        console.log(greatNum);
-        if(greatNum > numbers[count]){
-            greatNum = numbers[count];   
-        } 
-    } return greatNum;
+function min(numbers) {
+  let greatNum = numbers[0];
+  let count = 0;
+  while(count < numbers.length - 1){
+    count++;
+
+    if(greatNum > numbers[count]){
+      greatNum = numbers[count];   
+    } 
+  } return greatNum;
+}
+console.log(min(arr));
+  
+  
+  
+  
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function average(numbers) {
+  let sum = 0;
+  numbers.forEach(number =>  {
+    sum += number;
+
+    return sum / numbers.length;   
+  });
+}
+  
+console.log(average(array));
+  
+
+function repeat(fn, n){
+  for (let i=0; i <= n; i++){
+    fn();
   }
-  console.log(min(arr));
-//   let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+}
 
-//   function average(numbers) {
-//       let sum = 0;
-//     numbers.forEach(number =>  {
-//         sum += number;
-//         return sum / numbers.length;   
-//     }); console.log(sum);
-//   }
-  
-//   average(array);
-  
-  
-//   function testFunctionWorks(fn, input, expected) {
-//     if (fn(input) === expected) {
-//       console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
-//       return true;
-//     } else {
-//       console.log(
-//         'FAILURE: `' +
-//           fn.name +
-//           '([' +
-//           input +
-//           '])` should be ' +
-//           expected +
-//           ' but was ' +
-//           fn(input)
-//       );
-//       return false;
-//     }
-//   }
-  
+function hello (){
+  console.log('Hello World');
+}
+
+function goodBye (){
+  console.log('Goodbye World');
+}
+
+repeat(hello, 5);
+repeat(goodBye, 5);
 
 
-//   (function runTests() {
-//     const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//     const correctAns1 = 5.5;
-//     const numList2 = [0, -1, 1];
-//     const correctAns2 = 0;
-  
-//     const testResults = [
-//       testFunctionWorks(average, numList1, correctAns1),
-//       testFunctionWorks(average, numList2, correctAns2),
-//     ];
-//     const numPassing = testResults.filter(function(result) {
-//       return result;
-//     }).length;
-//     console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
-//   })();
-  
-  
+function filter(arr, fn) {
+  // TASK: Define your function here
+  let newArr = [];
+  for (let i =0; i < arr.length; i++){
+    if(fn(arr[i])===true){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 
+}
 
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
 
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
 
-//   function testFunctionWorks(fn, input, expected) {
-//     if (fn(input) === expected) {
-//       console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
-//       return true;
-//     } else {
-//       console.log(
-//         'FAILURE: `' +
-//           fn.name +
-//           '([' +
-//           input +
-//           '])` should be ' +
-//           expected +
-//           ' but was ' +
-//           fn(input)
-//       );
-//       return false;
-//     }
-//   }
-  
-//   function testEmpty(fn) {
-//     if (fn([]) === null || fn([]) == undefined) {
-//       console.log(`SUCCESS: ${fn.name} works on empty arrays`);
-//       return true;
-//     } else {
-//       console.log(
-//         `FAILURE: ${fn.name} should return undefined or null for empty arrays`
-//       );
-//       return false;
-//     }
-//   }
-  
-//   (function runTests() {
-//     // we'll use the variables in our test cases
-//     const numList1 = [-5, 28, 98, -20013, 0.7878, 22, 115];
-//     const realMin1 = numList1[3];
-//     const realMax1 = numList1[6];
-//     const numList2 = [0, 1, 2, 3, 4];
-//     const realMin2 = numList2[0];
-//     const realMax2 = numList2[4];
-  
-//     const testResults = [
-//       testFunctionWorks(max, numList1, realMax1),
-//       testFunctionWorks(max, numList2, realMax2),
-//       testFunctionWorks(min, numList1, realMin1),
-//       testFunctionWorks(min, numList2, realMin2),
-//       testEmpty(max),
-//       testEmpty(min),
-//     ];
-  
-//     const numPassing = testResults.filter(function(result) {
-//       return result;
-//     }).length;
-//     console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
-//   })();
-  
