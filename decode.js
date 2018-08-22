b4e54662de11b7d6b058ba3a8d5e822d76a9dc1d
Chris Rudder <cbrudder84@gmail.com>
2018-08-21 21:32:19 -0400

    added forgotten test string

--- decode.js
+++ decode.js
@@ -1,3 +1,5 @@
-const decoded = inputArray.reduce((total, val) => val.length === 3 ? total += ' ' : total += val[val.length-1].toUpperCase(),'');
+const firstCode = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+
+const decoded = firstCode.split(' ').reduce((total, val) => val.length === 3 ? total += ' ' : total += val[val.length-1].toUpperCase(),'');
 
 console.log(decoded);
